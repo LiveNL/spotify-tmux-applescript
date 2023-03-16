55afa674fecab14cb9cb4d64a6cad032f50e2049
LiveNL <jordiwippert@live.nl>
2023-03-16 22:41:03 +0100

    Make addition of datetime optional

--- bin/install.sh
+++ bin/install.sh
@@ -10,10 +10,14 @@ do
       shift
       FILENAME="$1"
       ;;
+    --with-datetime)
+      WITH_DATETIME=true
+      ;;
     --help)
-      echo "Usage: $0 [--filename filename] [--help]"
+      echo "Usage: $0 [--filename filename] [--with-datetime] [--help]"
       echo "Adds the script required to call the spotify script to your conf."
-      echo "By default ~/.tmux.conf is used, use --filename for custom files"
+      echo "--filename: Use parameter to specify the name of the file the code should be added to. Default ~/.tmux.conf."
+      echo "--with-datetime: Add datetime info to your statusbar. Default false"
       exit 0
       ;;
     *)
@@ -29,10 +33,15 @@ then
   FILENAME="$DEFAULT_FILENAME"
 fi
 
-tmux_code="set-window-option -g status-right '\
-#($CURRENT_DIR/bin/spotify_artist_and_track.applescript)\
-  %d/%m/%y %H:%M\
-'"
+tmux_code="set-window-option -g status-right '#($CURRENT_DIR/bin/spotify_artist_and_track.applescript)"
+
+if [ "$WITH_DATETIME" = true ]
+then
+  datetime="%d/%m/%y %H:%M'"
+  tmux_code="$tmux_code $datetime"
+else
+  tmux_code="$tmux_code'"
+fi
 
 echo "Adding code to: $FILENAME"
 echo "$tmux_code" >> "$FILENAME"
